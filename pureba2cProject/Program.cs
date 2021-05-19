9e5fc62f7f4d3d248560e1ec3b1082bf6b2e4b1c
FabianCancino <fabian.cancino,a@gmail.com>
2021-05-19 16:10:04 -0400

    mod program, adding menu and product class

--- pureba2cProject/Program.cs
+++ pureba2cProject/Program.cs
@@ -6,7 +6,27 @@ namespace pureba2cProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("****************************************");
+            Console.WriteLine("* Hello, Welcome to CamBox Console App  * ");
+            Console.WriteLine("****************************************");
+            Console.WriteLine(" ");
+            Console.WriteLine("Please insert your data:");
+            Console.WriteLine(" ");
+            String nameManager;
+            String rutManager;
+            Console.WriteLine("Insert your Name:");
+            Console.WriteLine("Example: Fabian Cancino");
+            Console.WriteLine(" ");
+            nameManager = Console.ReadLine();
+            Console.WriteLine("Insert your Rut without the check digit:");
+            Console.WriteLine("Example: 20648620");
+            Console.WriteLine(" ");
+            rutManager = Console.ReadLine();
+            Console.WriteLine(".......");
+            Console.WriteLine($"Congrats, your account has been created, {nameManager}");
+            Console.Clear();
+            Menu menu = new Menu();
+            menu.MainMenu();
         }
     }
 }
---
+++ pureba2cProject/productTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pureba2cProject
+{
+    class productTest
+    {
+
+        public int idCounter = 1;
+        public int idProduct;
+        public bool isAvailable;
+
+        List<productTest> ProductList = new List<productTest>();
+
+        Menu menu = new Menu();
+
+        public productTest(int idProduct, bool isAvalible)
+        {
+            this.idProduct = idProduct;
+            this.IsAvailable = isAvailable;
+        }
+        public productTest()
+        {
+
+        }
+        public int IdProduct
+        {
+            get { return idProduct; }
+            set { idProduct = value; }
+        }
+        public bool IsAvailable
+        {
+            get { return isAvailable; }
+            set { isAvailable = value; }
+        }
+
+        public void Product()
+        {
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\ta - Add new product");
+            Console.WriteLine("\tq - Display products");
+            Console.WriteLine("\te - Return to main menu");
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    addP();
+                    break;
+                case "q":
+                    displayP();
+                    break;
+                case "e":
+                    Console.Clear();
+                    menu.MainMenu();
+                    break;
+            }
+
+            void addP()
+            {
+                Console.WriteLine($"N° products: {idCounter}");
+                ProductList.Add(new productTest(idCounter, true));
+                idCounter = idCounter + 1;
+                Console.WriteLine("Do you want to continue adding products? (y/n)");
+                switch (Console.ReadLine())
+                {
+                    case "y":
+                        addP();
+                        break;
+                    case "n":
+                        Console.Clear();
+                        Product();
+                        break;
+                }
+            }
+
+            void displayP()
+            {
+                foreach (var productTest in ProductList)
+                {
+                    Console.WriteLine($"Id product: {productTest.IdProduct}, Is Available: {productTest.IsAvailable}");
+                }
+                Console.Write("Press any key to close the product display...");
+                Console.ReadKey();
+                Console.Clear();
+                Product();
+            }
+
+        }
+    }
+}
