2591183fe41c2519befe7acbc176266ef0cde747
FabianCancino <fabian.cancino,a@gmail.com>
2021-05-19 15:45:37 -0400

    sup

--- pureba2cProject/Program.cs
+++ pureba2cProject/Program.cs
@@ -6,7 +6,7 @@ namespace pureba2cProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Niggers!");
         }
     }
 }
