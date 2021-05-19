1eb4786e9cbb810921634532acc02aaecc27f23c
FabianCancino <fabian.cancino,a@gmail.com>
2021-05-19 15:40:49 -0400

    Add project files.

---
+++ pureba2cProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pureba2cProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
